7e903527a9ba5d6dd1b272218ab7988c019f9e25
dilla <faradilasna@gmail.com>
2025-05-25 17:52:57 +0700

    ubah11

--- database/migrations/2025_05_09_164659_add_trigger_to_pkls_table.php
+++ database/migrations/2025_05_09_164659_add_trigger_to_pkls_table.php
@@ -3,71 +3,37 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class AddTriggerToPklsTable extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
-         // Trigger untuk insert PKL
-            DB::unprepared('
-            CREATE TRIGGER after_insert_siswa
-            AFTER INSERT ON siswas
+        // Trigger setelah insert ke tabel pkls
+        DB::unprepared('
+            CREATE TRIGGER after_pkl_insert
+            AFTER INSERT ON pkls
             FOR EACH ROW
             BEGIN
-                IF NEW.id_pkl IS NOT NULL THEN
-                    UPDATE siswas
-                    SET status_pkl = TRUE
-                    WHERE id = NEW.id;
-                END IF;
+                UPDATE siswas
+                SET status_lapor_pkl = TRUE
+                WHERE id = NEW.siswa_id;
             END;
-
         ');
 
-        // // Trigger untuk update PKL
-        // DB::unprepared('
-        //     CREATE TRIGGER update_status_pkl_after_update
-        //     AFTER UPDATE ON pkls
-        //     FOR EACH ROW
-        //     BEGIN
-        //         DECLARE new_status BOOLEAN;
-        //         SET new_status = (SELECT status_pkl FROM siswas WHERE id = NEW.siswa_id);
-        //         IF new_status = 1 THEN
-        //             UPDATE siswas SET status_pkl = 0 WHERE id = NEW.siswa_id;
-        //         ELSE
-        //             UPDATE siswas SET status_pkl = 1 WHERE id = NEW.siswa_id;
-        //         END IF;
-        //     END;
-        // ');
-
-        // // Trigger untuk delete PKL
-        // DB::unprepared('
-        //     CREATE TRIGGER update_status_pkl_after_delete
-        //     AFTER DELETE ON pkls
-        //     FOR EACH ROW
-        //     BEGIN
-        //         UPDATE siswas SET status_pkl = 0 WHERE id = OLD.siswa_id;
-        //     END;
-        // ');
-
-
-           //Triger status siswa
-    //        DB::unprepared('
-    //        CREATE TRIGGER update_status_pkl_after_insert
-    //        AFTER INSERT ON pkls
-    //        FOR EACH ROW
-    //        BEGIN
-    //            UPDATE siswas
-    //            SET status_pkl = true
-    //            WHERE id = NEW.siswa_id;
-    //        END;
-    //    ');
-       /// triger duplikat data
-
+        // Trigger setelah delete dari tabel pkls
+        DB::unprepared('
+            CREATE TRIGGER after_pkl_delete
+            AFTER DELETE ON pkls
+            FOR EACH ROW
+            BEGIN
+                UPDATE siswas
+                SET status_lapor_pkl = FALSE
+                WHERE id = OLD.siswa_id;
+            END;
+        ');
     }
 
     public function down(): void
     {
-        // Menghapus trigger jika migrasi dibatalkan
-        DB::unprepared('DROP TRIGGER IF EXISTS update_status_pkl_after_insert');
-      
+        DB::unprepared('DROP TRIGGER IF EXISTS after_pkl_insert');
+        DB::unprepared('DROP TRIGGER IF EXISTS after_pkl_delete');
     }
-}
+};
